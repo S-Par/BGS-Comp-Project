1a326e6e12121b7f1f2ca3760c8ff4ce4455d996
unknown <sidparmar0@gmail.com>
2019-10-23 13:52:45 +0530

    Added startscreen, login and register

--- maingame.cpp
+++ maingame.cpp
@@ -8,22 +8,35 @@
 #include <string.h>
 
 //Function prototypes
+void registerUser(User &player);
+void loginUser(User &player);
+void pacmanGame(User &player);
+void sortPac(Pacman pacm[]);
+void startGraphicsMode();
+void drawRegister(int);
+void drawLogin(int);
 void drawPac(int);
 void drawPong(int);
 void graphicsGameIntro(char[]);
-void pacmanGame(User player);
-void sortPac(Pacman pacm[]);
+void startScreen(char []);
 
 int main() {
 	clrscr();
-	// Make a User obj and register it
 	User player;
-	int counter = player.regUser();
-	//write user object to the file
-	if (counter == 0){
-		fstream user("userobj.txt", ios::app|ios::binary);
-		user.write((char *)&player, sizeof(player));
-		user.close();
+	int optionReply;
+	char option[10] = "register";
+	startScreen(option);
+	if (strcmp(option, "register")) {
+		optionReply = registerUser();
+		if (optionReply == 1){
+			startScreen(option);
+		}
+	}
+	else{
+		optionReply = loginUser();
+		if (optionReply == 1){
+			startScreen(option);
+		}
 	}
 	/*
 	fstream userfile("userobj.txt", ios::in|ios::binary);
@@ -57,7 +70,7 @@ int main() {
 }
 
 //Function for implementing the game pacman
-void pacmanGame(User player) {
+void pacmanGame(User &player) {
 
 	//introduction
 	introduction();
@@ -121,7 +134,7 @@ void pacmanGame(User player) {
 	file.close();
 	*/
 	//Change back to app soon
-	fstream output("pacscore.txt", ios::app | ios::binary);
+	fstream output("pacscore.txt", ios::out | ios::binary);
 	output.write((char *)&pac, sizeof(pac));
 	output.close();
 
@@ -130,6 +143,97 @@ void pacmanGame(User player) {
 		cout<<pac.getName()<<" "<<pac.getScore()<<endl;
 	}
 	input.close();
+}
+
+// Registers a user object, if unsuccessful returns 1
+int registerUser(User &player){
+	int counter = player.regUser();
+	// If counter = 0, user registered successfully
+	// Write user obj to file
+	if (counter == 0){
+		fstream user("userobj.txt", ios::app|ios::binary);
+		user.write((char *)&player, sizeof(player));
+		user.close();
+		return 0;
+	}
+	else{
+		return 1;
+	}
+}
+
+//Logs in a user, if unsuccessful returns 1
+int loginUser(User &player){
+	char usrname[40], pwd[40], pwdCh = '2';
+	cout<<"\nEnter username:";
+	cin>>usrname;
+	// Password entry
+	int i = 0;
+	cout<<"Enter password(39 characters or below):";
+	strcpy(pwd, acceptPassword());
+	// Open user file to check
+	fstream user("userobj.txt", ios::in|ios::binary);
+	int successfulLogin = 0;
+	while (user.read((char*)&player, sizeof(player))) {
+		// Checks if username is taken
+		if (strcmp(player.getUsername(), usrname) == 0) {
+			user.close();
+			if (player.verifyPassword(pwd) == "True"){
+				cout<<"\nYou have logged in successfully!";
+				successfulLogin++;
+			}
+			break;
+		}
+	}
+	user.close();
+	if (successfulLogin == 0){
+		cout<<"\nSorry, details didn't match!";
+		delay(2000);
+		return 1;
+	}
+}
+
+//Displays the start screen
+void startScreen(char activeOp[]){
+	startGraphicsMode();
+	//Moves to x = 250, y = 150 to display title text
+	moveto(270,150);
+	settextjustify(CENTER_TEXT, CENTER_TEXT);
+	settextstyle(SANS_SERIF_FONT, HORIZ_DIR, 3);
+	outtext("Welcome to ");
+	moverel(120, 0);
+
+	settextstyle(TRIPLEX_FONT, HORIZ_DIR, 3);
+	rectangle(335, 135, 375, 175);
+	setcolor(RED);
+	outtext("Pr OTON");
+
+	// Login and Register options
+	settextstyle(SANS_SERIF_FONT, HORIZ_DIR, 1);
+	drawRegister(1);
+	drawLogin(0);
+
+	//Instructions for selecting an option
+	outtextxy(350, 350, "Toggle by pressing w(to go up) or s(to go down)");
+	outtextxy(350, 375, "    Press enter to select the option");
+
+	//Take user input till the user presses enter, toggle between pacman and pong
+	char ch = 1;
+	while (ch != 13) {
+		ch = getch();
+		if (ch == 'w'){
+			drawRegister(1);
+			drawLogin(0);
+			strcpy(activeOp, "register");
+		}
+		else if (ch == 's'){
+			drawRegister(0);
+			drawLogin(1);
+			strcpy(activeOp, "login");
+		}
+	}
+
+	cleardevice();
+	closegraph();
 }
 
 //sorts an array of pac objects in descending order of scores
@@ -148,10 +252,10 @@ void sortPac(Pacman pacm[]) {
 
 void drawPac(int active) {
 
-		setcolor(BLACK);
-		setfillstyle(SOLID_FILL, BLACK);
-		fillellipse(200, 200, 60, 60);
-		outtextxy(175, 200, "PACMAN");
+	setcolor(BLACK);
+	setfillstyle(SOLID_FILL, BLACK);
+	fillellipse(200, 200, 60, 60);
+	outtextxy(175, 200, "PACMAN");
 	if (active) {
 		setcolor(BLACK);
 		setfillstyle(SOLID_FILL, YELLOW);
@@ -177,10 +281,10 @@ void drawPong (int active) {
 	poly[6] = 400;
 	poly[7] = 260;
 
-		setcolor(BLACK);
-		setfillstyle(SOLID_FILL, BLACK);
-		fillpoly(4, poly);
-		outtextxy(445, 200, "PONG");
+	setcolor(BLACK);
+	setfillstyle(SOLID_FILL, BLACK);
+	fillpoly(4, poly);
+	outtextxy(445, 200, "PONG");
 	if (active) {
 		setcolor(BLACK);
 		setfillstyle(SOLID_FILL, WHITE);
@@ -195,30 +299,25 @@ void drawPong (int active) {
 	}
 }
 
+//Introduction in graphics
 void graphicsGameIntro(char activeGame[]) {
 
-	// request auto detection
-	int gdriver = DETECT, gmode, errorcode;
-
-	// initialize graphics and local variables
-	initgraph(&gdriver, &gmode, "C:\\TC\\BGI");
-
-	// read result of initialization
-	errorcode = graphresult();
-
-	//if an error occurred
-	if (errorcode != grOk)
-	{
-		printf("Graphics error: %s\n", grapherrormsg(errorcode));
-		printf("Press any key to halt:");
-		getch();
-		exit(1);
-	}
-
+	startGraphicsMode();
+	/*
+	Text styles:
+	char *fname[] = { "DEFAULT font",
+		"TRIPLEX font",
+		"SMALL font",
+		"SANS SERIF font",
+		"GOTHIC font"
+	};
+	*/
 	//display text
-	outtextxy(50, 50, "Toggle by pressing a(to go left) or d(to go right)");
-	outtextxy(50, 75, "press enter to select the active game");
+	settextstyle(SANS_SERIF_FONT, HORIZ_DIR, 1);
+	outtextxy(100, 50, "Toggle by pressing a(to go left) or d(to go right)");
+	outtextxy(100, 75, "    Press enter to select the active game");
 	//draw the circle for pacman and square for pong
+	settextstyle(DEFAULT_FONT, HORIZ_DIR, 1);
 	drawPac(1);
 	drawPong(0);
 	//Take user input till the user presses enter, toggle between pacman and pong
@@ -238,4 +337,77 @@ void graphicsGameIntro(char activeGame[]) {
 	}
 	cleardevice();
 	closegraph();
+}
+
+// Draws the register box in start screen
+void drawRegister(int active) {
+	int regPoly[8];
+	regPoly[0] = 280;
+	regPoly[1] = 200;
+	regPoly[2] = 380;
+	regPoly[3] = 200;
+	regPoly[4] = 380;
+	regPoly[5] = 230;
+	regPoly[6] = 280;
+	regPoly[7] = 230;
+
+	if (active) {
+		setcolor(BLACK);
+		setfillstyle(SOLID_FILL, WHITE);
+		fillpoly(4, regPoly);
+		outtextxy(335, 210, "Register");
+	}
+	else {
+		setcolor(WHITE);
+		setfillstyle(SOLID_FILL, BLACK);
+		fillpoly(4, regPoly);
+		outtextxy(335, 210, "Register");
+	}
+}
+
+// Draws the login box in start screen
+void drawLogin(int active) {
+	int logPoly[8];
+	logPoly[0] = 280;
+	logPoly[1] = 250;
+	logPoly[2] = 380;
+	logPoly[3] = 250;
+	logPoly[4] = 380;
+	logPoly[5] = 280;
+	logPoly[6] = 280;
+	logPoly[7] = 280;
+
+	if (active) {
+		setcolor(BLACK);
+		setfillstyle(SOLID_FILL, WHITE);
+		fillpoly(4, logPoly);
+		outtextxy(330, 260, "Login");
+	}
+	else {
+		setcolor(WHITE);
+		setfillstyle(SOLID_FILL, BLACK);
+		fillpoly(4, logPoly);
+		outtextxy(330, 260, "Login");
+	}
+}
+
+//switches on graphics mode
+void startGraphicsMode(){
+    // request auto detection
+	int gdriver = DETECT, gmode, errorcode;
+
+	// initialize graphics and local variables
+	initgraph(&gdriver, &gmode, "C:\\TC\\BGI");
+
+	// read result of initialization
+	errorcode = graphresult();
+
+	//if an error occurred
+	if (errorcode != grOk)
+	{
+		printf("Graphics error: %s\n", grapherrormsg(errorcode));
+		printf("Press any key to halt:");
+		getch();
+		exit(1);
+	}
 }
