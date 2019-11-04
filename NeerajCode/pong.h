2c5a503546763b156ecfb65a38c39ac0094f6d3e
unknown <neerajjm12345@gmail.com>
2019-11-04 11:46:51 +0530

    Changes...

--- NeerajCode/pong.h
+++ NeerajCode/pong.h
@@ -14,13 +14,23 @@
 const int startPaddleTopYCo = 200;
 const int leftPaddleXco = 10;
 const int rightPaddleXco = 620;
+const int startBallYCo = 250;
+const int startBallXCo = 300;
+const int RadBall=10;
 //Global Variables:
 int player1score = 0;
 int player2score = 0;
+int ntempx=-4;
+int velx=4;
+int tempx=4;
+int vely=-4;
+char score[1]="1";
+int tempballx=300;
 
+int tempbally=250;
 
 class Paddle{
-    //xCo is x coordinate of the left edge of the paddle 
+    //xCo is x coordinate of the left edge of the paddle
     //thickness is 5
     int xCo;
     int topYCo;
@@ -41,6 +51,57 @@ class Paddle{
     void moveUp();
     // Moves the paddle down
     void moveDown();
+	//sets the y-coordinate
+	void setY(int newY){
+		topYCo = newY;
+		bottomYCo = topYCo + 100;
+	}
+};
+
+class Ball{
+	// x coordinate of the ball
+	int BallXCo;
+	// y coordinate of the ball
+	int BallYCo;
+	// x component of velocity
+	int velX;
+	// y component of velocity
+	int velY;
+	public:
+	//Constructer for ball
+	Ball();
+	//Destructor
+	~Ball();
+	//Draws the ball at current coordinates
+	void drawBall();
+	//Erases the ball
+	void eraseBall();
+	//Moves the ball;
+	void MoveBall();
+	// Returns BallX
+	int getX(){
+		return BallXCo;
+	}
+	// Returns BallY
+	int getY(){
+		return BallYCo;
+	}
+	// Sets the x-coordinate
+	void setX(int newX){
+		BallXCo = newX;
+	}
+	// Sets the y-coordinate
+	void setY(int newY){
+		BallYCo = newY;
+	}
+	// Sets velX
+	void setVelX(int newVelX){
+		velX = newVelX;
+	}
+	//Sets velY
+	void setVelY(int newVelY){
+		velY = newVelY;
+	}
 };
 
 Paddle::Paddle(int startXCo){
@@ -55,9 +116,9 @@ Paddle::~Paddle(){
 
 int Paddle::isColliding (int ballXCo, int ballYCo){
     if (ballXCo + 10 >= xCo && ballXCo + 10 <= xCo + 15){
-        if (ballYCo >= topYCo && ballYCo <= bottomYCo){
-            return 1;
-        }
+		if (ballYCo >= topYCo && ballYCo <= bottomYCo){
+			return 1;
+		}
     }
     return 0;
 }
@@ -88,6 +149,35 @@ void Paddle::moveUp(){
     drawPaddle();
 }
 
+
+Ball::Ball(){
+	BallXCo = startBallXCo;
+	BallYCo = startBallYCo;
+	velX = 4;
+	velY = 4; 
+}
+Ball::~Ball(){
+}
+Ball::drawBall(int color){
+	setcolor(color);
+	setlinestyle(SOLID_LINE, 1, 1);
+	setfillstyle(SOLID_FILL, BLACK);
+	circle(BallXCo, BallYCo, RadBall);
+}
+Ball::eraseBall(){
+	setcolor(BLACK);
+	setfillstyle(SOLID_FILL, BLACK);
+	circle(BallXCo, BallYCo, RadBall);
+}
+Ball::moveBall(){
+	eraseBall();
+	BallXCo+=velx;
+	BallYCo+=vely;
+	drawBall(0);
+}
+
+
+
 // Draws the map (but not the paddles or ball)
 void drawMap(){
     setcolor(WHITE);
@@ -104,6 +194,83 @@ void drawMap(){
 	outtextxy(85, 25, arr1);
 	sprintf(sc2, "%d", player2score);
 	outtextxy(435, 25, arr2);
+}
+
+void reset(Ball &ball, Paddle &leftPad, Paddle &rightPad){
+	// Erase ball and paddles
+	ball.eraseBall();
+	leftPad.erasePaddle();
+	rightPad.erasePaddle();
+	// Reset each paddle back to start Yco
+	leftPad.setY(startPaddleTopYCo);
+	rightPad.setY(startPaddleTopYCo);
+	// Reset ball to start coordinates
+	ball.setX(startBallXCo);
+	ball.setY(startBallYCo);
+	// Reset velocity to random values 
+	//counters
+	int a, b;
+	a = random(2);
+	b = random(2);
+	if (a){
+		ball.setVelX(4);
+	}
+	else {
+		ball.setVelX(-4);
+	}
+	if (b){
+		ball.setVelY(4);
+	}
+	else {
+		ball.setVelY(-4);
+	}
+	//Draw everything:
+	leftPad.drawPaddle();
+	rightPad.drawPaddle();
+	ball.drawBall();
+}
+
+void Text()
+{
+	settextstyle(TRIPLEX_FONT, HORIZ_DIR, 8);
+	setcolor(4);
+
+	outtextxy(195,25, "PONG");
+
+	//Prints the rules
+	setcolor(10);
+	settextstyle(SIMPLEX_FONT, HORIZ_DIR, 5);
+	outtextxy(10,120, "*Rules");
+
+	setcolor(14);
+	settextstyle(TRIPLEX_SCR_FONT,HORIZ_DIR,3);
+
+	outtextxy(10, 175, "->Player 1 press W arrow to move the bar up ");
+	outtextxy(10, 225, "->Press S arrow to move the bar down");
+	outtextxy(10, 275, "->Player 2 press up arrow to move the bar up");
+	outtextxy(10, 325, "->Press Down arrow to move the bar down ");
+	outtextxy(10, 375, "Press any key to start the game");
+
+}
+
+void Game()
+{
+	Text();
+	getch();
+	cleardevice();
+	drawMap();
+	Paddle LeftPaddle(10);
+	Paddle RightPaddle(620);
+	Ball ball;
+	ball.drawBall();
+	LeftPaddle.drawPaddle();
+	RightPaddle.drawPaddle();
+	while (1)
+	{
+		ball.MoveBall();
+		LeftPaddle.isColliding(ball.getX(), ball.getY());
+		RightPaddle.isColliding(ball.getX(), ball.getY());
+	}
 }
 
 #endif
